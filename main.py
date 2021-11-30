be21697404e3444cec1cfcf1c21404067fb8799a
Social network -Django-Python <sg.sapir@gmail.com>
2021-11-30 06:38:58 +0200

    Reorgenize functions
    Drill down to SimCSE project, and implement functions on main level

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
 
 import torch
-from scipy.spatial.distance import cosine
 from transformers import AutoModel, AutoTokenizer
-from simcse import SimCSE
+from sklearn.metrics.pairwise import cosine_similarity
 
 import argparse
 import logging as log
 import json
 import sys
 import os
-
-# Import the model and tokenizer. The package will download the model automatically
-tokenizer = AutoTokenizer.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
-model = SimCSE("princeton-nlp/sup-simcse-bert-base-uncased")
+import numpy
 
 # Declare constants
-# For full run results
+# Input sentences as corpus
 INPUT_SENTENCES_FILE = 'full_sentences.json'  # Optional files are available in data directory
 SENTENCES_INPUT_FILE_PATH = 'data\\full_sentences.json'
 
-# For specific similarity check from the user
+# Output for each check done
 OUTPUT_SENTENCES_FILE = 'Sentences_similarity_log.csv'
 SENTENCES_OUTPUT_FILE_PATH = 'output\\Sentences_similarity_log.csv'
 HEADER_ALL = '\nSentence similarity log'
 
-# For specific similarity check of two sentences
-OUTPUT_SPECIFIC_SIMILARITY_FILE = 'Specific_sentences_similarity_grade.csv'
-SPECIFIC_SIMILARITY_SENTENCES_OUTPUT_FILE_PATH = 'output\\Specific_sentences_similarity_grade.csv'
-HEADER_SPECIFIC = '\nCheck specific sentences similarity score'
-
 # Similarity evaluation score
 THRESHOLDS = 0.7620
+
+# Import the model and tokenizer. The package will download the model automatically
+tokenizer = AutoTokenizer.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
+model1 = AutoModel.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
 
 
 def parse_args(sentences=None):
@@ -39,7 +34,6 @@ def parse_args(sentences=None):
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--sentences", help="Prints the supplied argument")
-
     parsed_args = parser.parse_args(sys.argv[1:])
 
     return parsed_args
@@ -80,7 +74,7 @@ def input_source(argv):
 
 def load_sentences(input_sentences):
     # Initiate a list with the input sentences
-    
+
     sentences = []
     with open(input_sentences) as file:
         for line in json.load(file):
@@ -90,14 +84,57 @@ def load_sentences(input_sentences):
     return sentences
 
 
-def build_sentences_corpus(sentences):
-    # build index for a group of sentences
+def tokenize_input(sentence):
+    # Convert text to transformer-readable token IDs.
 
-    model.build_index(sentences)
+    return tokenizer(sentence, padding=True, truncation=True, return_tensors="pt")
+
+
+def encoding(sentences):
+    # Replace the build and encode sentence
+    batch_size = 64
+    single_sentence = False
+
+    # For Usr input sentence
+    if isinstance(sentences, str):
+        sentences = [sentences]
+        single_sentence = True
+
+    # start embedding
+    embedding_list = []
+    with torch.no_grad():
+        total_batch = len(sentences) // batch_size + (1 if len(sentences) % batch_size > 0 else 0)
+        for batch_id in range(total_batch):
+            inputs = tokenize_input(sentences[batch_id * batch_size:(batch_id + 1) * batch_size])
+            # truncate to tuple
+            inputs = {k: v for k, v in inputs.items()}
+
+            # Start embedding
+            embeddings = model1(**inputs, return_dict=True).pooler_output
+            embeddings = embeddings / embeddings.norm(dim=1, keepdim=True)
+            embedding_list.append(embeddings)
+            
+    embeddings = torch.cat(embedding_list, 0)
+
+    # For User input
+    if single_sentence:
+        embeddings = embeddings[0]
+
+    # Convert data type to numpy
+    if not isinstance(embeddings, numpy.ndarray):
+        embeddings = embeddings.numpy()
+
+    # Save the encoded sentences to sentences corpus
+    if not single_sentence:
+        log.info("Building index...")
+        model1.index = {"sentences": sentences}
+        model1.index["index"] = embeddings
+
+    return embeddings
 
 
 def user_sentence():
-    # Input a sentence from user
+    # User input sentence
 
     user_preferences = input("Enter a phrase: ")
 
@@ -107,100 +144,97 @@ def user_sentence():
     return user_preferences
 
 
-def encode_user_phrase(user_preference):
-    # Encode specific sentence
+def evaluate_cousin_score(query_vectors, corpus_vectors):
+    # returns an N*M similarity array: format of: index in list: score
 
-    return model.encode(user_preference)
-
-
-def embedding_sentences(inputs):
-    # encoding sentences into embeddings
-
-    model1 = AutoModel.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
-
-    # Get the embeddings
-    with torch.no_grad():
-        embeddings = model1(**inputs, output_hidden_states=True, return_dict=True).pooler_output
-
-    return embeddings
+    return cosine_similarity(query_vectors, corpus_vectors).tolist()
 
 
-def evaluate_sentences_similarities():
-    # Grade two sentence similarity
+def pull_passed_thresholds(similarities):
+    # Return the index and score of sentences who their score pass the THRESHOLDS
 
-    model1 = AutoModel.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
+    id_and_score_pass = []
+    for sentence_index, sentence_score in enumerate(similarities):
+        if sentence_score >= THRESHOLDS:
+            # Verify score upper the the set THRESHOLDS
+            id_and_score_pass.append((sentence_index, sentence_score))
 
-    texts = []
-    final_list = []
-
-    texts.append(user_sentence())
-    texts.append(user_sentence())
-
-    inputs = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
-
-    # Get the embeddings
-    embeddings = embedding_sentences(inputs)
-    
-    # Use directly the cosin func that compare the encoded vectors
-    for i in range(1, len(embeddings)):
-        # Cosine similarities are in [-1, 1]. Higher means more similar
-        
-        most_similar = 1 - cosine(embeddings[i], embeddings[i - 1])
-        final_list.append(texts[i] + " : " + texts[i - 1] + ' scored: ' + str(most_similar))
-
-    # Print and save to file the specific similarity check
-    main_dir = os.path.dirname(os.path.abspath(OUTPUT_SPECIFIC_SIMILARITY_FILE))
-    input_specific_sentences = os.path.join(main_dir, SPECIFIC_SIMILARITY_SENTENCES_OUTPUT_FILE_PATH)
-    review_outputs(final_list, input_specific_sentences, HEADER_SPECIFIC)
+    return id_and_score_pass
 
 
-def find_similarity(user_preference):
+def generated_text_of_passed_thresholds(id_and_score_pass):
+    # From the index, retrieve sentence text, format of: text: score
+
+    return [(model1.index["sentences"][idx], score) for idx, score in id_and_score_pass]
+
+
+def find_similarity(to_search_embeddings, corpus_embeddings):
     # search a sentence among the encoded group of sentences
-    
-    # 'search' methd contains the encoded phase and the cosin phase 
-    result = model.search(user_preference)
 
-    return result
+    pass_results = []
+    id_and_score_pass = []
+    query_vectors = to_search_embeddings
+    corpus_vectors = corpus_embeddings
+
+    # Handle batch of sentences or single sentence
+    single_query, single_corpus = len(query_vectors.shape) == 1, len(corpus_vectors.shape) == 1
+    if single_query:
+        query_vectors = query_vectors.reshape(1, -1)
+    if single_corpus:
+        corpus_vectors = corpus_vectors.reshape(1, -1)
+
+    # Retrieve the similarity score of each corpus's sentence to the searched one
+    similarities = evaluate_cousin_score(query_vectors, corpus_vectors)
+
+    if single_query:
+        id_and_score_pass = pull_passed_thresholds(similarities[0])
+        if single_corpus:
+            id_and_score_pass = pull_passed_thresholds(float(similarities[0]))
+
+    # Retrieve similarities found texts from vectors
+    if id_and_score_pass:
+        pass_results = generated_text_of_passed_thresholds(id_and_score_pass)
+
+    return pass_results
 
 
-def score_to_boolean_rate(input_text, output_text):
+def score_to_boolean_rate(user_preference, pass_similarity):
     # Rate scored similarity to: 1 - pass THRESHOLD, 0 - not pass THRESHOLD
 
     pairs_similarity = []
 
-    if input_text is None:
-        log.warning('No sentence received, can not rate the similarity')
-
-    for current_sentence in output_text:
-        if list(current_sentence)[1] >= THRESHOLDS:
+    if not len(pass_similarity):
+        # The context founded, was not pass in the corpus
+        pairs_similarity.append("\'{input}\' sentence was not found, in the thresholds bounders, "
+                                "rated score is: 0".format(input=user_preference))
+    else:
+        for current_sentence in pass_similarity:
+            # Optional more then one match
             # similarity found, Optional multiple similarities sentences
             pairs_similarity.append("\'{input}\', is similar to \'{output}\', rated score is: 1".
-                                    format(input=input_text, output=list(current_sentence)[0]))
-        else:
-            # The context founded, was not pass in the corpus
-            pairs_similarity.append("A \'{input}\' sentence was not found, in the thresholds bounders, "
-                                    "rated score is: 0".format(input=input_text))
+                                    format(input=user_preference, output=list(current_sentence)[0]))
 
     return pairs_similarity
 
 
-def review_outputs(final_texts_by_score, archive, header=None):
+def review_outputs(final_texts_by_score):
     # Save output to a file
 
-    archive = open(archive, "a")
-    archive.write(header)
+    main_dir = os.path.dirname(os.path.abspath(OUTPUT_SENTENCES_FILE))
+    input_sentences = os.path.join(main_dir, SENTENCES_OUTPUT_FILE_PATH)
+
+    archive = open(input_sentences, "a")
+    archive.write(input_sentences)
     for match in final_texts_by_score:
         archive.write('\n')
-        if isinstance(match, int) or isinstance(match, float):
-            archive.write(str(match))
-        else:
-            archive.write(''.join(match))
+        archive.write(''.join(match))
         print(match)
 
     archive.close()
 
 
 def main(argv):
+
     # Handle standard input parameters
     args = parse_args(argv)
 
@@ -211,25 +245,22 @@ def main(argv):
     sentences = load_sentences(input_sentences)
 
     # Build index for the input sentences - encode the group of sentences
-    build_sentences_corpus(sentences)
+    corpus_embeddings = encoding(sentences)
 
     # User sentence input
     user_preference = user_sentence()
 
+    # Build index for the input sentences - encode the User input
+    to_search_embeddings = encoding(user_preference)
+
     # Find most similar sentence in the encoded sentences from the loaded file
-    most_similar = find_similarity(user_preference)
+    pass_similarity = find_similarity(to_search_embeddings, corpus_embeddings)
 
     # Rate similarity pair sentences to pass (=1) not pass(=0) thresholds
-    final_texts_by_score = score_to_boolean_rate(user_preference, most_similar)
-
-    # Calculate cosine similarities
-    # Turn to Note due it was not required in the assignment
-    # evaluate_sentences_similarities()
+    final_texts_by_score = score_to_boolean_rate(user_preference, pass_similarity)
 
     # Printout and archives final results
-    main_dir = os.path.dirname(os.path.abspath(OUTPUT_SENTENCES_FILE))
-    input_sentences = os.path.join(main_dir, SENTENCES_OUTPUT_FILE_PATH)
-    review_outputs(final_texts_by_score, input_sentences, HEADER_ALL)
+    review_outputs(final_texts_by_score)
 
 
 if __name__ == '__main__':
